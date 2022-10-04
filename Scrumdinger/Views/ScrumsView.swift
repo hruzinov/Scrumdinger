285e74ffb6632d1a970e83eef32e9907669b22d3
Evhen Gruzinov <evhen@gruzinov.com>
2022-10-04 15:15:01 +0300

    Added DetailView and navigations

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -9,8 +9,16 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.theme.mainColor)
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button(action: {}) {
+                Image(systemName: "plus")
             }
         }
     }
@@ -18,6 +26,8 @@ struct ScrumsView: View {
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
     }
 }
