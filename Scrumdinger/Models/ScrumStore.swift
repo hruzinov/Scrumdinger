bb49946747660ed78d15ff2b33976fa7134975db
Evhen Gruzinov <evhen@gruzinov.com>
2022-10-05 21:07:34 +0300

    New functionality: saving scrums to phone and loading it on relaunching

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,58 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileUrl() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+    
+    static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileUrl = try fileUrl()
+                guard let file = try? FileHandle(forReadingFrom: fileUrl) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(dailyScrums))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try JSONEncoder().encode(scrums)
+                let outfile = try fileUrl()
+                try data.write(to: outfile)
+                DispatchQueue.main.async {
+                    completion(.success(scrums.count))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
+
