8bdace578fd6c1ba8b14261515b1c19885bfd3bc
Evhen Gruzinov <evhen@gruzinov.com>
2022-10-06 20:42:12 +0300

    New functionality: error handling

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -7,6 +7,8 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
@@ -15,33 +17,23 @@ struct ScrumdingerApp: App {
                         do {
                             try await ScrumStore.save(scrums: store.scrums)
                         } catch {
-                            fatalError("Error saving scrums.")
+                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
                         }
                     }
-//                    ScrumStore.save(scrums: store.scrums) { result in
-//                        if case .failure(let error) = result {
-//                            fatalError(error.localizedDescription)
-//                        }
-//                    }
                 })
             }
             .task {
                 do {
                     store.scrums = try await ScrumStore.load()
                 } catch {
-                    fatalError("Error loading scrums.")
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
                 }
             }
-//            .onAppear {
-//                ScrumStore.load { result in
-//                    switch result {
-//                    case .failure(let error):
-//                        fatalError(error.localizedDescription)
-//                    case .success(let scrums):
-//                        store.scrums = scrums
-//                    }
-//                }
-//            }
+            .sheet(item: $errorWrapper, onDismiss: {
+                store.scrums = DailyScrum.sampleData
+            }) { wrapper in
+                ErrorView(errorWrapper: wrapper)
+            }
         }
     }
 }
